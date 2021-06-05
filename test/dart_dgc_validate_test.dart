7b12027507c71f62da9cef0bcbfd7c7b60854b71
Jeroen Trappers <jeroen.trappers@gmail.com>
2021-06-05 11:32:08 +0200

    Local testing

--- test/dart_dgc_validate_test.dart
+++ test/dart_dgc_validate_test.dart
@@ -2,11 +2,18 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:asn1lib/asn1lib.dart';
+import 'package:flutter/foundation.dart';
 import 'package:test/test.dart';
 import 'package:convert/convert.dart';
 import 'package:cbor/cbor.dart';
 import 'package:dart_base45/dart_base45.dart';
 import 'package:dart_cose/dart_cose.dart';
+import 'package:x509b/x509.dart';
+
+
+const _KID_HEADER = 4;
+
 
 List<int> unChain(String input) {
   // trim HC1
@@ -52,11 +59,11 @@ String extractKid(List<int> cose) {
   var headerList = headers.getDecodedData();
   if (headerList != null) {
     var header = headerList.first;
-    kidBuffer = header[HeaderParameters['kid']];
+    kidBuffer = header[_KID_HEADER];
   }
 
   // kid could not be retreived from protected header.
-  kidBuffer ??= unprotectedHeader[HeaderParameters['kid']];
+  kidBuffer ??= unprotectedHeader[_KID_HEADER];
   var kid = Uint8List.view(kidBuffer.buffer, 0, kidBuffer.length);
   if (kid.length > 8) {
     kid = kid.sublist(0, 8);
@@ -70,6 +77,76 @@ String extractKid(List<int> cose) {
 }
 
 var zlib = ZLibCodec();
+
+dynamic toDart(ASN1Object obj) {
+  if (obj is ASN1Null) return null;
+  if (obj is ASN1Sequence) return obj.elements.map(toDart).toList();
+  if (obj is ASN1Set) return obj.elements.map(toDart).toSet();
+  if (obj is ASN1Integer) return obj.valueAsBigInteger;
+  if (obj is ASN1ObjectIdentifier) return ObjectIdentifier.fromAsn1(obj);
+  if (obj is ASN1BitString) return obj.stringValue;
+  if (obj is ASN1Boolean) return obj.booleanValue;
+  if (obj is ASN1OctetString) return obj.stringValue;
+  if (obj is ASN1PrintableString) return obj.stringValue;
+  if (obj is ASN1UtcTime) return obj.dateTimeValue;
+  if (obj is ASN1IA5String) return obj.stringValue;
+  if (obj is ASN1UTF8String) return obj.utf8StringValue;
+  if( obj.valueBytes().isNotEmpty){
+  switch (obj.tag) {
+    case 0xa0:
+      return toDart(ASN1Parser(obj.valueBytes()).nextObject());
+    case 161:
+      return toDart(ASN1Parser(obj.valueBytes()).nextObject());
+    case 162:
+      return toDart(ASN1Parser(obj.valueBytes()).nextObject());
+    case 0x86:
+      return utf8.decode(obj.valueBytes());
+  }
+  }
+  throw ArgumentError(
+      'Cannot convert $obj (${obj.runtimeType}) to dart object.');
+}
+
+printSeq(ASN1Sequence seq){
+  print("SEQUENCE (${seq.elements.length})");
+  seq.elements.forEach((element){
+    printObject(element);
+  });
+}
+
+printSet(ASN1Set set){
+  print("SET (${set.elements.length})");
+  set.elements.forEach((element) {
+    printObject(element);
+  });
+}
+
+printObjectIdentifier(ASN1ObjectIdentifier oid){
+  print("OBJECT IDENTIFIER");
+  ObjectIdentifier objectIdentifier = ObjectIdentifier.fromAsn1(oid);
+  print(objectIdentifier.toString() + " (${oid.identifier})");
+}
+
+printObject(ASN1Object obj){
+  print("OBJECT with tag: ${obj.tag}");
+  try{
+    var d = toDart(obj);
+    print(d);
+  }
+  on Error catch(e){
+    print(e);
+  }
+  if( obj is ASN1ObjectIdentifier){
+    printObjectIdentifier(obj as ASN1ObjectIdentifier);
+  }
+  if( obj is ASN1Set ){
+    printSet(obj as ASN1Set);
+  }
+  if( obj is ASN1Sequence ){
+    printSeq(obj as ASN1Sequence);
+  }
+
+}
 
 void main() {
   var dir = Directory('dgc-testdata');
@@ -261,4 +338,86 @@ void main() {
       }
     });
   });
+
+
+
+
+
+  test('trust', () async {
+    var trustrootpem = '''-----BEGIN CERTIFICATE-----
+MIIFQTCCAymgAwIBAgIULTspcOgGgEEdJECdg/XQe4gDp5kwDQYJKoZIhvcNAQEL
+BQAwUjELMAkGA1UEBhMCQkUxFzAVBgNVBAoTDkV1cm9wZWFuIFVuaW9uMSowKAYD
+VQQDEyFEaWdpdGFsIEdyZWVuIENlcnRpZmljYXRlIEdhdGV3YXkwHhcNMjEwNTIw
+MDczMjMzWhcNMjIwNTIwMDczMjMzWjBSMQswCQYDVQQGEwJCRTEXMBUGA1UEChMO
+RXVyb3BlYW4gVW5pb24xKjAoBgNVBAMTIURpZ2l0YWwgR3JlZW4gQ2VydGlmaWNh
+dGUgR2F0ZXdheTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBALO1AtMH
+57pU/PfFvqdJwIMPCiavPizzl0V6ehDugn13USn2iKO2MUcucL2m5tc8AnqhIXtu
+gitGai43llHF0qq39uCmuK5GKjmi4tu9aVOTypPxRaRSjmiuZrEtcL0AAj9x93t5
+DKjyXsTez6buldrVjpwFTlUcwbTxZfEgDRx+Bgt4U3x/1ZBjIjETGU2c3AlztqeB
+yxYeaBi6lfa5FNRdhPkD6T/OkiadLmLllhGTnSHQZMpi8oYl0f+IawOnmx3d4AtF
+zWIi8TGCwaCtv87AvYBwnTy3ufR81KvN2GPneEo2whY3+y7SNY2IWMTYhluQ8mDr
+3bn+xRRLBF5JQVttqLCAtQj8pb219E65xETegncDtE65uVmOgZmQkBwvtIoyxQMw
+K7sK+2ZuARk7cLVOddVxIkQ19EAEsumlYN9gS02iIE06BtszeVMQ9iFn4sZu6n7J
+i7gECxven1CtaXuCuRvsTco7LOlnrvcpSiwFKqskh0sEpYffGJkaP74xJup4EG8Q
+cYZA1PREXyceQFL7uvrLSWSk5pMxUq/fU9bBE7quLQrUIIC8JfGgQFTv/rxjPvE/
+Pbf5qMZsEdLcNBkY/eEt9vlZ6InrY6VEBurzWAAp+GHA9b8M4o/7vqwYwItOxtnm
+tCAHK9DHFnumONig26ibqJ/kd2gIBylmvO7rAgMBAAGjDzANMAsGA1UdDwQEAwIH
+gDANBgkqhkiG9w0BAQsFAAOCAgEAlM01JmCqeBHevidzJdnOcywIaoEOzGnWJ8rg
+DWOhIam5y2kxGQsdYUE6ssLMXp3msMf9K86itBn1UX2bhV/OvvVeVMi5FRaIjRkn
+b2+U3MhSgOffvJtqRtMGwQY4dDGyqt/M5ktEl3INNSklli9tUm8CtgAYEcJhSmVr
+G0DSlXb9B7esbZcYSqfuGx8nP+mwhflfH10v8GQcYKwUNN2KHL3/6SP15WfvXPPr
+y+gGKLw8gOwa1b9EYiNGCFJANERL+SuF64AZZFBEN4PjD5ZA8M53pmM8DiDB8ism
+F7DYX0j1AZECHr9DG1lco4LhRf3GHYcVYLYDDNBm/HVp7klt4JwWGYzDsstzY133
+JgPUPjs38qj+yBGgolgVyaaz2PIGy7S9VBVN2JLlsCHFqWZoNSTWM8JSL+7XI9F+
+zgdf0n9Fx0sBMcDKt+InOb9YHxbPMnHaVlznEpaIJJjgpyZQ3tHWnxsOCyY9sXgc
+ezfqBCauJi63YNAPgyDvE4P9HGRwdcxMAjgwQm3k2zfHT/fhcrX8inM2wFXQ/AFS
+n0JTcCKXd4el35J3drLnfkV4r/aV/zt8C+WfLdjGaBx1rBB4tgeHeXAO/Tt6MDla
+Z/AjYumq/z7rP/EMHdEpamouG+c/lCNdZA30jqDEIh+DYlNjo/ByDjqhRe3/+bg/
+K3XfzZs=
+-----END CERTIFICATE-----''';
+
+    var httpClient = HttpClient();
+      var request = await httpClient.getUrl(Uri.parse('https://cert-app.be/trustList/DSC.json'));
+      var response = await request.close();
+      var dscBytes = await consolidateHttpClientResponseBytes(response);
+      var DSCs = jsonDecode(utf8.decode(dscBytes));
+
+      request = await httpClient.getUrl(Uri.parse('https://cert-app.be/trustList/CSCA.json'));
+      response = await request.close();
+      var cscaBytes = await consolidateHttpClientResponseBytes(response);
+      var CSCAs = jsonDecode(utf8.decode(cscaBytes));
+
+      expect(DSCs, isA<List>());
+      expect(CSCAs, isA<List>());
+
+      X509Certificate trustRoot = parsePem(trustrootpem).first;
+    print('trustRoot rawData parsed:');
+    print(trustRoot);
+
+      CSCAs.where((csca) => csca['country'] == 'BE').forEach((csca){
+
+
+          X509Certificate cscaCert = X509Certificate.fromAsn1(ASN1Sequence.fromBytes(base64Decode(csca['rawData'])));
+          print('cscaCert rawData parsed:');
+          print(cscaCert);
+
+          var p = ASN1Parser(base64Decode(csca['signature']));
+          var i = 0;
+          while (p.hasNext()) {
+            i++;
+            var o1 = p.nextObject();
+
+            printObject(o1);
+
+
+          }
+
+      });
+
+
+
+
+  });
+
+
 }
